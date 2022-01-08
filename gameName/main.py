cf685e1d6f3c4162955c1cf9259bcd05005d268b
theonej203 <96204142+theonej203@users.noreply.github.com>
2022-01-08 17:48:29 -0500

    More template update

--- gameName/main.py
+++ gameName/main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.constants import KEYDOWN, KEYUP
 
 pygame.init()
 
@@ -12,12 +13,14 @@ pygame.display.set_caption("PlaceHolder")
 
 clock = pygame.time.Clock()
 
-window.fill((255,255,255)) 
+window.fill((0,0,0)) 
 
 
 def start():
     run = True
 
+    up = down = left = right = False
+    shoot = False
 
     while run:
         
@@ -26,6 +29,70 @@ def start():
 
             if event.type == pygame.QUIT:
                 run = False
+
+            if event.type == KEYDOWN:
+
+                if event.key == pygame.K_w:
+                    up = True
+                    print("w")
+
+                if event.key == pygame.K_s:
+                    down = True
+                    print("s")
+
+                if event.key == pygame.K_a:
+                    left = True
+                    print("a")
+
+                if event.key == pygame.K_d:
+                    right = True
+                    print("d")
+
+                if event.key == pygame.K_SPACE:
+                    shoot = True
+                    print("space")
+
+
+            if event.type == KEYUP:
+
+                if event.key == pygame.K_w:
+                    up = False
+                    print("w")
+
+                if event.key == pygame.K_s:
+                    down = False
+                    print("s")
+
+                if event.key == pygame.K_a:
+                    left = False
+                    print("a")
+
+                if event.key == pygame.K_d:
+                    right = False
+                    print("d")
+
+                if event.key == pygame.K_SPACE:
+                    shoot = False
+                    print("space")
+
+
+        if up:
+            print("up")
+
+        if down:
+            print("down")
+
+        if left:
+            print("left")
+
+        if right:
+            print("right")
+
+        if shoot:
+            print("shoot")
+
+
+
 
 
         pygame.display.flip()
