5fd1a6c0edac7596aebc1954944d06426fab1595
marcobantoli <97365844+marcobantoli@users.noreply.github.com>
2022-01-08 19:42:48 -0500

    Create Ninja.py
    The ninja class (not finished). Loading the image still needs to be coded.

---
+++ gameName/Ninja.py
@@ -0,0 +1,22 @@
+import pygame
+
+#Put in whatever ninja image we are using
+IMAGE = pygame.image.load()
+
+class Ninja(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = IMAGE
+        self.rect = self.image.get_rect()
+
+    def move_up(self, units):
+        self.rect.y = self.rect.y - units
+
+    def move_down(self, units):
+        self.rect.y = self.rect.y + units
+
+    def move_right(self, units):
+        self.rect.x = self.rect.x + units
+
+    def move_left(self, units):
+        self.rect.x = self.rect.x - units     
